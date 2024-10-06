200dfa214609ab25cb2266f5b9a9841bd76dd244
ChiriVulpes <github@chiri.works>
2024-10-06 05:24:57 +0000

    Deploy e0c8e5b: v2.0.3, respect path.sep

--- CLI.js
+++ CLI.js
@@ -35,7 +35,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
         File.children = children;
         function relative(file) {
             file = path_1.default.relative(process.cwd(), file);
-            return file.startsWith(".") ? file : `.\\${file}`;
+            return file.startsWith(".") ? file : `.${path_1.default.sep}${file}`;
         }
         File.relative = relative;
     })(File || (File = {}));
