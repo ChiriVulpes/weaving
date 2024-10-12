3400c64ee4e9d2fee520617a6e2e05685698e894
ChiriVulpes <github@chiri.works>
2024-10-12 20:28:40 +0000

    Deploy da842bc: Fix DEFAULT_WARPS Lib export no longer working

--- Lib.js
+++ Lib.js
@@ -7,19 +7,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./QuiltTransformer", "./Weave"], factory);
+        define(["require", "exports", "./Quilt", "./QuiltTransformer"], factory);
     }
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.stream = exports.createQuiltTransformer = exports.createFileTransformer = exports.QuiltTransformer = exports.DEFAULT_WARPS = void 0;
+    const Quilt_1 = __importDefault(require("./Quilt"));
     const QuiltTransformer_1 = __importDefault(require("./QuiltTransformer"));
     exports.QuiltTransformer = QuiltTransformer_1.default;
-    const Weave_1 = __importDefault(require("./Weave"));
     const { create, createFileTransformer, stream } = QuiltTransformer_1.default;
     exports.createQuiltTransformer = create;
     exports.createFileTransformer = createFileTransformer;
     exports.stream = stream;
-    const { DEFAULT_WARPS } = Weave_1.default;
+    const { DEFAULT_WARPS } = Quilt_1.default;
     exports.DEFAULT_WARPS = DEFAULT_WARPS;
 });
