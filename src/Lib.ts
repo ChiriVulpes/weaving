da842bcdd46a8ab09b6ad897d5ce4522ad041b81
Chiri Vulpes <chiri@chiri.works>
2024-10-13 09:28:07 +1300

    Fix DEFAULT_WARPS Lib export no longer working

--- src/Lib.ts
+++ src/Lib.ts
@@ -1,8 +1,8 @@
+import Quilt from "./Quilt"
 import QuiltTransformer from "./QuiltTransformer"
-import Weave from "./Weave"
 
 const { create, createFileTransformer, stream } = QuiltTransformer
-const { DEFAULT_WARPS } = Weave
+const { DEFAULT_WARPS } = Quilt
 
 export { DEFAULT_WARPS, QuiltTransformer, createFileTransformer, create as createQuiltTransformer, stream }
 
